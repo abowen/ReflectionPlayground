2e4f372ca46a20f02e93853808a8b2d417ff9127
abowen <andrew.james.bowen@gmail.com>
2014-06-06 22:03:27 +1000

    First commit that loads all local assemblies

---
+++ ReflectionPlayground/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ReflectionPlayground
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        {
+            var path = Directory.GetCurrentDirectory();
+            var assemblies = Directory.GetFiles(path, "*.dll").Select(Assembly.LoadFile);
+            var assembliesOutput = string.Join("\r\n", assemblies);
+            AssemblyTextBox.Text = assembliesOutput;
+        }
+    }
+}
